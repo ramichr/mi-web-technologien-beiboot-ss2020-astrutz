17698f82c62f7aed3b15ae86454503e5d8710120
astrutz <alex-strutz@t-online.de>
2020-04-04 15:51:12 +0200

    Return an empty http page on root

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const PORT = 3000;
 const app = express();
 
+const rootRouter = require('./route/root');
+
+app.use('/', rootRouter);
+app.set('views', __dirname + '/view');
+app.set('view engine', 'ejs');
 
 app.listen(PORT, function () {
     console.log('Server started on port', PORT);
 });
+
+module.exports = app;
