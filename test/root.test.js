0593fc9fd0825de4309b27ae41e34bdd75ebe5ac
astrutz <alex-strutz@t-online.de>
2020-04-06 17:31:30 +0200

    Testing assets

--- test/root.test.js
+++ test/root.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('../app.js');
 const assert = require('assert');
 
-describe('HTTP Requests', function () {
-    describe('GET /', function () {
+describe('HTTP GET /', function () {
+    describe('Load site', function () {
         it('should return 200 status code', function () {
             return request(app)
                 .get('/')
@@ -13,6 +13,18 @@ describe('HTTP Requests', function () {
             return request(app)
                 .get('/')
                 .expect('Content-Type', 'text/html; charset=utf-8');
+        });
+    });
+    describe('Load assets', function () {
+        it('should load the CSS file', function () {
+            return request(app)
+                .get('/root.css')
+                .expect(200);
+        });
+        it('should load the JS file', function () {
+            return request(app)
+                .get('/root.js')
+                .expect(200);
         });
     });
 });
--- test/upload.test.js
+++ test/upload.test.js
@@ -2,8 +2,8 @@ const request = require('supertest');
 const app = require('../app.js');
 const assert = require('assert');
 
-describe('HTTP Requests', function () {
-    describe('GET /upload', function () {
+describe('HTTP GET /upload', function () {
+    describe('Load site', function () {
         it('should return 200 status code', function () {
             return request(app)
                 .get('/upload')
@@ -13,6 +13,18 @@ describe('HTTP Requests', function () {
             return request(app)
                 .get('/upload')
                 .expect('Content-Type', 'text/html; charset=utf-8');
+        });
+    });
+    describe('Load assets', function () {
+        it('should load the CSS file', function () {
+            return request(app)
+                .get('/upload.css')
+                .expect(200);
+        });
+        it('should load the JS file', function () {
+            return request(app)
+                .get('/upload.js')
+                .expect(200);
         });
     });
 });
