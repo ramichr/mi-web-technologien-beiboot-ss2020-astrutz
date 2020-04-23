28367d2d24e71dc3173cd43270a549eba674057c
astrutz <alex-strutz@t-online.de>
2020-04-23 15:34:21 +0200

    Remove material js

--- web/root.js
+++ web/root.js
@@ -1,7 +1,3 @@
-window.onload = () =>{
-    const MDCRipple = mdc.ripple.MDCRipple;
-    const buttons = [].map.call(document.querySelectorAll('.mdc-button'), function(el) {
-        return new MDCRipple(el);
-    });
+window.onload = () => {
 };
 
