37b52d4d8b550c34b9d5f3db4b8e4b9149df30aa
astrutz <--replace-all>
2020-08-10 19:14:04 +0200

    Use API to get images

--- dev/app.js
+++ dev/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const PORT = process.env.PORT || 4000;
 const app = express();
 const cors = require('cors');
+const axios = require('axios');
 
 app.use('/', express.static('dist'));
 app.use(cors());
+
+app.get('/api', async (req, res) => {
+    const images = await axios.get('http://host.docker.internal:3000' + req.url);
+    res.send(images.data);
+});
 
 app.listen(PORT, function () {
     console.log('Enviroment started');
