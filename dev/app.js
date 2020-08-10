2a7f115cfd026c609bd27948b15963202c2a792c
astrutz <--replace-all>
2020-08-10 19:52:44 +0200

    Use lint within dev container

--- dev/app.js
+++ dev/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 4000;
 const app = express();
 const cors = require('cors');
@@ -8,12 +9,12 @@ app.use('/', express.static('dist'));
 app.use(cors());
 
 app.get('/api', async (req, res) => {
-    const images = await axios.get('http://host.docker.internal:3000' + req.url);
-    res.send(images.data);
+  const images = await axios.get(`http://host.docker.internal:3000${req.url}`);
+  res.send(images.data);
 });
 
-app.listen(PORT, function () {
-    console.log('Enviroment started');
+app.listen(PORT, () => {
+  console.log('Enviroment started');
 });
 
 module.exports = app;
--- dev/reload.js
+++ dev/reload.js
@@ -1,41 +1,41 @@
 const { spawn } = require('child_process');
+
 const babel = spawn('npx', ['babel', 'src/js/main.js', '--watch', '--out-file', 'dist/main.min.js']);
-const sass = spawn('sass', ['--style=compressed','--watch', 'src/css/style.scss:dist/style.min.css']);
-const app = spawn('nodemon', ['--legacy-watch', 'app.js']);
+const sass = spawn('sass', ['--style=compressed', '--watch', 'src/css/style.scss:dist/style.min.css']);
+const app = spawn('nodemon', ['app.js', '--legacy-watch', '--exec', 'eslint src/js/* && node']);
 
 babel.stdout.on('data', (stdout) => {
-    console.log(`Babel: ${stdout}`);
+  console.log(`Babel: ${stdout}`);
 });
 
 babel.stderr.on('data', (stderr) => {
-    console.error(`Babel: ${stderr}`);
+  console.error(`Babel: ${stderr}`);
 });
 
 babel.on('close', (code) => {
-    console.log(`Babel closed (${code})`);
+  console.log(`Babel closed (${code})`);
 });
- 
+
 sass.stdout.on('data', (stdout) => {
-    console.log(`Sass: ${stdout}`);
+  console.log(`Sass: ${stdout}`);
 });
 
 sass.stderr.on('data', (stderr) => {
-    console.error(`Sass: ${stderr}`);
+  console.error(`Sass: ${stderr}`);
 });
 
 sass.on('close', (code) => {
-    console.log(`Sass closed (${code})`);
+  console.log(`Sass closed (${code})`);
 });
 
 app.stdout.on('data', (data) => {
-    console.log(`App: ${data}`);
+  console.log(`App: ${data}`);
 });
 
 app.stderr.on('data', (data) => {
-    console.error(`App: ${data}`);
+  console.error(`App: ${data}`);
 });
 
 app.on('close', (code) => {
-    console.log(`App closed (${code})`);
+  console.log(`App closed (${code})`);
 });
-
